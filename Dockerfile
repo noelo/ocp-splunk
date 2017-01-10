dac2de2c6217954ab7717f30d677f6151e69cfb5
Noel OConnor <noel.oconnor@gmail.com>
2017-01-10 20:16:44 +0000

    Added to git

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM centos:latest
+MAINTAINER noconnor@redhat.com
+
+WORKDIR /tmp
+
+RUN yum update -y && yum install -y wget
+
+RUN wget -O splunkforwarder-6.5.1-f74036626f0c-linux-2.6-x86_64.rpm 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=6.5.1&product=universalforwarder&filename=splunkforwarder-6.5.1-f74036626f0c-linux-2.6-x86_64.rpm&wget=true' && rpm -ihv splunkforwarder-6.5.1-f74036626f0c-linux-2.6-x86_64.rpm
+
+WORKDIR /opt/splunkforwarder/bin/
+
+COPY entrypoint.sh /sbin/entrypoint.sh
+RUN chmod +x /sbin/entrypoint.sh
+
+#RUN mkdir -p /tmp/splunk/
+#RUN chmod a+rw /tmp/splunk
+#ENV SPLUNK_SERVER=
+#ENV SPLUNK_MONITOR_LOCN=/tmp
+
+ENTRYPOINT ["/sbin/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/sh 
+echo "Pushing from $LOG_FILES to $SPLUNK_SERVER" 
+
+/opt/splunkforwarder/bin/splunk start --accept-license
+if [ $? -eq 0 ]
+then
+  echo "Successfully started splunk universal forwarder"
+else
+  echo "Failed to start universal forwarder " >&2
+  exit 1
+fi
+
+/opt/splunkforwarder/bin/splunk add forward-server $SPLUNK_SERVER -auth admin:changeme
+if [ $? -eq 0 ]
+then
+  echo "Successfully registered with splunk remote instance "
+else
+  echo "Failed to register to remote splunk instance " >&2
+  exit 1
+fi
+
+/opt/splunkforwarder/bin/splunk add monitor $SPLUNK_MONITOR_LOCN
+if [ $? -eq 0 ]
+then
+  echo "Successfully added monitor of $SPLUNK_MONITOR_LOCN"
+  exit 0
+else
+  echo "Failed to monitor $SPLUNK_MONITOR_LOCN" >&2
+  exit 1
+fi
+
+tail -n 0 -f /opt/splunkforwarder/var/log/splunk/splunkd_stderr.log &
+wait
